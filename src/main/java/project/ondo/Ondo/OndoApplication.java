a7aebbc66af8698c8b2cdb305f4f675d47df16ec
황지훈 <wangjajihooon@gmail.com>
2025-10-30 20:39:14 +0900

    init

---
+++ src/main/java/project/ondo/Ondo/OndoApplication.java
@@ -0,0 +1,13 @@
+package project.ondo.Ondo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/ondo/Ondo/OndoApplicationTests.java
@@ -0,0 +1,13 @@
+package project.ondo.Ondo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
